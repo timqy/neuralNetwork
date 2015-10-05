b05c61221187cb796167bc5e8e3c7d03a88fe762
aous <dv13thg@cs.umu.se>
2015-10-05 15:32:31 +0000

    added main

---
+++ src/main/Perceptron.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ * Created by aous on 10/5/15.
+ */
+public class Perceptron {
+    /**
+     * Main function, will create and run the robot.
+     *
+     * @param args the
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
